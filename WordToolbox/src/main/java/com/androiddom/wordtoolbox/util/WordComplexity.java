a9a88e820667f8a2e0e05f482646dfcfa04b6ab8
Dominic <dawinkel@gmail.com>
2016-01-19 21:28:43 -0800

    Adding the calculation for word complexity.

--- WordToolbox/src/main/java/com/androiddom/wordtoolbox/util/WordComplexity.java
+++ WordToolbox/src/main/java/com/androiddom/wordtoolbox/util/WordComplexity.java
@@ -5,4 +5,49 @@ package com.androiddom.wordtoolbox.util;
  */
 public class WordComplexity {
 
+	/**
+	 * Method to get the complexity of a word.
+	 * 
+	 * @param input
+	 *            The word to calculate the complexity for.
+	 * @return The calculated complexity for the input.
+	 */
+	public static int getWordComplexity(String input) {
+		int wordComplexity = 0;
+
+		wordComplexity += input.length();
+		wordComplexity += StringUtils.getRoughSyllableCount(input);
+		wordComplexity += getSoundexComplexity(input);
+
+		return wordComplexity;
+	}
+
+	/**
+	 * A method used to calculate the complexity of the Soundex-like
+	 * representation of the input.
+	 * 
+	 * @param input
+	 *            The input to use for the calculation.
+	 * @return The complexity for the Soundex-like calculation.
+	 */
+	private static int getSoundexComplexity(String input) {
+		int soundexComplexity = 0;
+
+		String soundexString = PhoneticUtils.getSoundex(input);
+
+		String prev = null;
+		for (int i = 0; i < soundexString.length(); i++) {
+			String curChar = soundexString.substring(i, i + 1);
+
+			// Do not count repeated sounds
+			if (prev == null || !curChar.equals(prev)) {
+				soundexComplexity++;
+			}
+
+			prev = curChar;
+		}
+
+		return soundexComplexity;
+	}
+
 }
