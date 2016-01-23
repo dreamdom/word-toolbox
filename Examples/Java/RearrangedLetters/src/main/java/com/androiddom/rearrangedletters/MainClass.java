deb0f080e135d3ca368bf06639a35743b1c1edfd
Dominic <dawinkel@gmail.com>
2016-01-23 12:10:39 -0800

    Work in progress on the ReArranged Letters example application

--- Examples/Java/RearrangedLetters/src/main/java/com/androiddom/rearrangedletters/MainClass.java
+++ Examples/Java/RearrangedLetters/src/main/java/com/androiddom/rearrangedletters/MainClass.java
@@ -1,11 +1,24 @@
 package com.androiddom.rearrangedletters;
 
+import java.io.File;
+
+import com.androiddom.wordtoolbox.Dictionary;
+
 public class MainClass {
 
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Rearranged Letters");
-
+		
+		Dictionary dictionary = new Dictionary.Builder(new File("../../../DictionaryData/dictionary.txt"))
+				.filterPossesive()
+				.filterProper()
+				.minLength(4)
+				.build();
+		
+		System.out.println(dictionary.getRandomWord());
+		
+		
 	}
 
 }
