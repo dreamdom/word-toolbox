c3b3694aaa39b6166a30e9fce83caf429076263e
Dominic <dawinkel@gmail.com>
2016-01-22 22:47:21 -0800

    Adding the rearranged letters example application.

---
+++ Examples/Java/RearrangedLetters/src/main/java/com/androiddom/rearrangedletters/MainClass.java
@@ -0,0 +1,11 @@
+package com.androiddom.rearrangedletters;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Rearranged Letters");
+
+	}
+
+}
