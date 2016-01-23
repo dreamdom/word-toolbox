3e5ecf2df7d2f02ca78b8582b5562261052c01f6
Dominic <dawinkel@gmail.com>
2016-01-23 12:07:19 -0800

    Adding unit test for the Dictionary

--- WordToolbox/src/test/java/com/androiddom/wordtoolbox/DictionaryTest.java
+++ WordToolbox/src/test/java/com/androiddom/wordtoolbox/DictionaryTest.java
@@ -1,16 +1,30 @@
 package com.androiddom.wordtoolbox;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class DictionaryTest {
+	
+	/**
+	 * The dictionary object to use in the tests.
+	 */
+	private static Dictionary testDictionary;
+	
+	@BeforeClass
+	public static void setUp() {
+		File file = new File("../DictionaryData/dictionary.txt");
+		testDictionary = new Dictionary.Builder(file).build();
+	}
 
 	@Test
-	public void test() {
-		// Stub test
-		int x = 1;
-		assertTrue("x not equal to 1", x==1);
+	public void hasWordTest() {
+		assertTrue("'the' not in dictionary", testDictionary.hasWord("the"));
+		assertFalse("'asdf' in dictionary", testDictionary.hasWord("asdf"));
 	}
 
 }
