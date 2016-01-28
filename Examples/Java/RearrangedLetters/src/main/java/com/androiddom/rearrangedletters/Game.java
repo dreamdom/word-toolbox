b836d14a09e2f94930f0d091e9cd8c3cca4ac2bc
Dominic <dawinkel@gmail.com>
2016-01-27 22:35:38 -0800

    Updates to the rearranged letters project.

--- Examples/Java/RearrangedLetters/src/main/java/com/androiddom/rearrangedletters/Game.java
+++ Examples/Java/RearrangedLetters/src/main/java/com/androiddom/rearrangedletters/Game.java
@@ -11,12 +11,24 @@ import com.androiddom.wordtoolbox.MutibleDictionary;
  */
 public class Game {
 
+	/**
+	 * An integer that corresponds to the minimum complexity for a game.
+	 */
 	private static final int BASE_WORD_COMPLEXITY = 6;
 
 	private MutibleDictionary dictionary;
 	private int gameLevel;
 	private Random random;
 
+	/**
+	 * Constructor for the game object.
+	 * 
+	 * @param dictionary
+	 *            A dictionary to use for the game.
+	 * @param gameLevel
+	 *            What level the game should be played at. Higher levels
+	 *            correspond to a higher level of difficulty.
+	 */
 	public Game(MutibleDictionary dictionary, int gameLevel) {
 		this.gameLevel = gameLevel;
 
@@ -24,21 +36,30 @@ public class Game {
 		random = new Random();
 
 		// Filter the dictionary down to the desired complexity
-		this.dictionary = new MutibleDictionary.Builder(dictionary)
-				.complexity(gameLevelToWordComplexity())
-				.build();
+		this.dictionary = new MutibleDictionary.Builder(dictionary).complexity(gameLevelToWordComplexity()).build();
 	}
 
+	/**
+	 * Method to create a new round in the game.
+	 * 
+	 * @return A Round object.
+	 */
 	public Round newRound() {
-		
+
 		String word = dictionary.getRandomWord();
-		
+
 		// Remove the word from the dictionary so it won't get used again
 		dictionary.removeWord(word);
-		
+
 		return new Round(word, random);
 	}
 
+	/**
+	 * Method to convert the set game complexity to a word complexity used
+	 * internally.
+	 * 
+	 * @return A word complexity level.
+	 */
 	private int gameLevelToWordComplexity() {
 		return BASE_WORD_COMPLEXITY + gameLevel;
 	}
--- Examples/Java/RearrangedLetters/src/main/java/com/androiddom/rearrangedletters/Round.java
+++ Examples/Java/RearrangedLetters/src/main/java/com/androiddom/rearrangedletters/Round.java
@@ -1,5 +1,6 @@
 package com.androiddom.rearrangedletters;
 
+import java.util.List;
 import java.util.Random;
 
 import com.androiddom.wordtoolbox.util.StringUtils;
@@ -11,12 +12,13 @@ import com.androiddom.wordtoolbox.util.StringUtils;
  *
  */
 public class Round {
-	
+
 	public static final int MAX_REARRANGE_TRIES = 100;
 
 	private final String word;
 	private String rearrangedWord;
 	private Random random;
+	private int hintCount;
 
 	public Round(String word, Random random) {
 		// Save a reference to the original word
@@ -26,19 +28,99 @@ public class Round {
 		// again
 		this.random = random;
 
+		// Initialize the hint count to 0
+		hintCount = 0;
+
 		// Rearrange the word!
-		
-		// TODO: Make sure it is not the same!
-		this.rearrangedWord = StringUtils.rearrangeString(word, random);
-		
+		this.rearrangedWord = rearrangeWord(word);
+
 	}
-	
+
 	public String getRearrangedWord() {
 		return rearrangedWord;
 	}
-	
+
 	public String getWord() {
 		return word;
+	}
+
+	/**
+	 * Method to check if an input is the same as the original word.
+	 * 
+	 * @param input
+	 *            The input to check.
+	 * @return True if the input is the same, false otherwise.
+	 */
+	public boolean checkWord(String input) {
+		return input.equals(word);
+	}
+
+	/**
+	 * The round is over if the rearranged word is equal to the real word. At
+	 * this point there is nothing left to do in the round.
+	 * 
+	 * @return true if the round is over, false otherwise.
+	 */
+	public boolean isRoundOver() {
+		return this.rearrangedWord.equals(word);
+	}
+
+	/**
+	 * Method to get the number of hints used in the round.
+	 * 
+	 * @return The number of hints used in the round.
+	 */
+	public int getHintsUsed() {
+		return hintCount;
+	}
+	
+	public int getScore() {
+		return word.length() - hintCount - 1;
+	}
+
+	public void useHint() {
+		// increment the hint count
+		hintCount++;
+
+		// No more hints to give, the original word is revealed
+		if (hintCount >= word.length() - 1) {
+			this.rearrangedWord = word;
+			return;
+		}
+
+		// Convert the original word to a character list
+		List<Character> characterList = StringUtils.stringToCharacterList(word);
+
+		StringBuilder hintBuilder = new StringBuilder();
+		StringBuilder rearrangedBuilder = new StringBuilder();
+
+		// Build up the hint
+		for (int i = 0; i < hintCount; i++) {
+			hintBuilder.append(characterList.get(i));
+		}
+
+		// Build up the part that should still be rearranged
+		for (int i = hintCount; i < word.length(); i++) {
+			rearrangedBuilder.append(characterList.get(i));
+		}
+
+		// Put together the rearranged word as a combination of the hint and
+		// rearranged part
+		this.rearrangedWord = hintBuilder.toString() + rearrangeWord(rearrangedBuilder.toString());
+	}
+
+	// private methods
+
+	private String rearrangeWord(String input) {
+		String rearrangeWord = input;
+		int count = 0;
+
+		while (rearrangeWord.equals(input) && count < MAX_REARRANGE_TRIES) {
+			rearrangedWord = StringUtils.rearrangeString(word, random);
+			count++;
+		}
+
+		return rearrangeWord;
 	}
 
 }
