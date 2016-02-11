44aa1a6e5542bbc14efefc6fbf56ddfad0c5d36a
Dominic <dawinkel@gmail.com>
2016-02-10 21:14:20 -0800

    Adding stubs for two more testing files.

---
+++ WordToolbox/src/test/java/com/androiddom/wordtoolbox/analytics/DictionaryAnalyticsTest.java
@@ -0,0 +1,9 @@
+package com.androiddom.wordtoolbox.analytics;
+
+/**
+ * A set of test cases for the DictionaryAnalytics functions.
+ *
+ */
+public class DictionaryAnalyticsTest {
+
+}
---
+++ WordToolbox/src/test/java/com/androiddom/wordtoolbox/analytics/LetterAnalyticsTest.java
@@ -0,0 +1,9 @@
+package com.androiddom.wordtoolbox.analytics;
+
+/**
+ * A set of test cases for the LetterAnalytics functions.
+ *
+ */
+public class LetterAnalyticsTest {
+
+}
